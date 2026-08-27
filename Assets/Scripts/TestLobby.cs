7cc1fd0c846f3ab9c304be71988af780c9621b4d
Mithran2003 <mithransaini5@gmail.com>
2026-08-27 18:19:27 +0530

    added create lobbie and List lobbies

--- Assets/Scripts/TestLobby.cs
+++ Assets/Scripts/TestLobby.cs
@@ -1,16 +1,53 @@
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 
 public class TestLobby : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Lobby hostLobby;
+    private async void Start()
     {
-        
+        await UnityServices.InitializeAsync();
+        AuthenticationService.Instance.SignedIn+=()=>
+        {
+            Debug.Log("Signed in "+AuthenticationService.Instance.PlayerId);
+        };
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
-    // Update is called once per frame
-    void Update()
+    private async void Createlobby()
     {
-        
+        try
+        {
+            string lobbyName="MyLobby";
+            int maxPlayers = 4;
+            Lobby lobby= await LobbyService.Instance.CreateLobbyAsync(lobbyName,maxPlayers);
+            hostLobby = lobby;
+            Debug.Log($"Created Lobby! {lobby.Name} {lobby.MaxPlayers}");
+        }
+        catch(LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+            
+    }
+
+    private async void ListLobbie() 
+    {
+        try
+        {
+            QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync();
+            Debug.Log("Lobbies Found: "+queryResponse.Results.Count);
+            foreach (Lobby lobby in queryResponse.Results)
+            {
+                Debug.Log(lobby.Name+" "+lobby.MaxPlayers);
+            }
+        }
+        catch(LobbyServiceException e)
+        {
+            Debug.Log(e);   
+        }
     }
 }
